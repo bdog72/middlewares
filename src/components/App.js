1e46a1acc2cfbedb33a3dcecc60aead338012f2d
Brian Bycynski <high5bri@hotmail.com>
2017-08-07 05:49:01 -0400

    added some bootstrap styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
-
+import UserList from './user_list'
 class App extends Component {
   render () {
-    return <div>
-      <h1>Hello, World!</h1>
-    </div>
+    return (
+      <div>
+        <UserList />
+      </div>
+    )
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { AppContainer } from 'react-hot-loader'
 import Redbox from 'redbox-react'
-
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
 import App from './components/App'
+import reducers from './reducers'
 
 import './styles/screen.scss'
+
+const createStoreWithMiddleware = applyMiddleware()(createStore)
 
 const root = document.getElementById('root')
 
@@ -16,7 +20,11 @@ const render = app => {
   )
 }
 
-render(<App />)
+render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>
+)
 
 if (module.hot) {
   module.hot.accept('./components/App', () => {
